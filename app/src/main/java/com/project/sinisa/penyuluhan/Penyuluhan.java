298c9e5d2c0cf88666b30e62d171925ce4ca036e
ludfyrahman <ludfyr@gmail.com>
2020-06-04 14:26:14 +0700

    design

---
+++ app/src/main/java/com/project/sinisa/penyuluhan/Penyuluhan.java
@@ -0,0 +1,16 @@
+package com.project.sinisa.penyuluhan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.project.sinisa.R;
+
+public class Penyuluhan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_penyuluhan);
+    }
+}
---
+++ app/src/main/java/com/project/sinisa/sewa/Sewa.java
@@ -0,0 +1,16 @@
+package com.project.sinisa.sewa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.project.sinisa.R;
+
+public class Sewa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sewa);
+    }
+}
