7523ff3afb31ae70d7630f00635ba1211d41123c
ludfyrahman <ludfyr@gmail.com>
2020-06-12 00:19:30 +0700

    update 20200612

--- app/src/main/java/com/project/sinisa/MainActivity.java
+++ app/src/main/java/com/project/sinisa/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.project.sinisa.akun.Sign_In;
+import com.project.sinisa.dashboard.Dashboard;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/project/sinisa/sewa/Form_Sewa.java
+++ app/src/main/java/com/project/sinisa/sewa/Form_Sewa.java
@@ -18,12 +18,10 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.project.sinisa.dashboard.Dashboard;
 import com.project.sinisa.R;
 import com.project.sinisa.config.AppController;
-import com.project.sinisa.config.AuthData;
 import com.project.sinisa.config.ServerAccess;
-import com.project.sinisa.dashboard.Dashboard;
-import com.project.sinisa.penyuluhan.Pengajuan_Jadwal;
 
 import org.json.JSONException;
 import org.json.JSONObject;
