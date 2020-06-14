706e711dc451f253ec91156a82b264405f504987
ludfyrahman <ludfyr@gmail.com>
2020-06-14 14:10:52 +0700

    drawable

--- app/src/main/java/com/project/sinisa/config/ServerAccess.java
+++ app/src/main/java/com/project/sinisa/config/ServerAccess.java
@@ -4,7 +4,7 @@ import java.text.DecimalFormat;
 
 public class ServerAccess {
 //    deklarasi untuk link link api yang dibutuhkan di aplikasi
-    public static final String BASE_URL = "http://sinisa.mif-project.com/old";
+    public static final String BASE_URL = "http://sinisa.mif-project.com/old/";
     public static final String ROOT_API = BASE_URL+"api/";
     public static final String LOGIN = ROOT_API+"login.php";
     public static final String REGISTER = ROOT_API+"register.php";
